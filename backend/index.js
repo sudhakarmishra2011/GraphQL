2b45faf143a913274a49955191e426f1df559426
Sudhakar <sudhakarmishra2011@gmail.com>
2024-05-31 20:13:02 +0530

    feat: Insert + Update + Delete

--- backend/index.js
+++ backend/index.js
@@ -8,24 +8,50 @@ const typeDefs = `
     name: String
     email: String
   }
+
   type Query {
     users: [User]
+    user(email : String): User
   }
 
   type Mutation {
     addUser(name: String!, email: String!): User
+    deleteUser(email : String) : User
+    updateUser(name : String, email : String) : User
   }
 `;
 
 const resolvers = {
   Query: {
     users: () => userModel.findAll(),
+
+    user: (_, { email }) => userModel.findOne({
+      where: {
+        email: email
+      }
+    })
   },
   Mutation: {
     addUser: async (_, { name, email }) => {
       const user = await userModel.create({ name, email });
       return user;
     },
+
+    deleteUser: async (_, { email }) => await userModel.destroy({
+      where: {
+        email: email
+      }
+    }),
+    
+    updateUser: async (_, { name, email }) => {
+      console.log(name, email)
+      const user = await userModel.update({ name }, {
+        where: {
+          email: email
+        }
+      })
+      return user
+    }
   },
 };
 
@@ -50,6 +76,6 @@ console.log(`🚀  Server ready at: ${url}`);
 
 Since we are using type = module to load ES6 modules.
 
-We have to metion .js as file extension while importing.
+We have to mention .js as file extension while importing.
 
 */
